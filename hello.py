da9c24dcdabad5d5ddbee8e7c257387ea072b658
IrasubizaBenyse <irabenise123@gmail.com>
2024-10-11 08:46:11 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("hello,python")
+name=("enter your age:")
+print("hello,"+name)
+age=input("enter your age:")
+age=float(age)#convert the output to an integer
+print(f"you will be{age}next year!")
