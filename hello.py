f6b4de13aee816bccf87d7f6e34432c106e213a4
IrasubizaBenyse <irabenise123@gmail.com>
2024-10-11 08:46:59 +0200

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-print("hello,python")
-name=("enter your age:")
-print("hello,"+name)
-age=input("enter your age:")
-age=float(age)#convert the output to an integer
-print(f"you will be{age}next year!")
